85c32a16c9159f21eaf37a383554012f3f916c7e
arokosandria <kmsa@interia.pl>
2023-11-06 11:53:39 +0100

    solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Main main = new Main();
         main.run(new Scanner(System.in));
     }
 
     public void run(Scanner scanner) {
-        // napisz swój program tutaj. Do wczytywania danych użyj przekazanego w parametrze scannera
+        List<Integer> numbers = MathematicOperations.numbersList(scanner);
+        if (MathematicOperations.checkNumbersList(numbers)) {
+            MathematicOperations.numbersReversal(numbers);
+            MathematicOperations.viewSum(numbers);
+            MathematicOperations.minNumbersView(numbers);
+            MathematicOperations.maxNumbersView(numbers);
+        }
     }
 }
---
+++ src/main/java/MathematicOperations.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MathematicOperations {
+    public static List<Integer> numbersList(Scanner scanner) {
+        List<Integer> integerList = new ArrayList<>();
+        System.out.println("Podaj liczby całkowite, wprowadzenie liczby ujemnej program zakonczy");
+        int numb;
+        while ((numb = scanner.nextInt()) >= 0) {
+            integerList.add(numb);
+        }
+        return integerList;
+    }
+
+    public static void numbersReversal(List<Integer> numbersList) {
+        for (int i = numbersList.size() - 1; i >= 0; i--) {
+            System.out.print(numbersList.get(i));
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static int sumNumbers(List<Integer> numbersList) {
+        int sum = 0;
+        for (Integer integer : numbersList) {
+            sum += integer;
+        }
+
+        return sum;
+    }
+
+    public static void viewSum(List<Integer> numbersList) {
+        for (int i = 0; i < numbersList.size(); i++) {
+            System.out.print(numbersList.get(i));
+            if (i < numbersList.size() - 1) {
+                System.out.print(" + ");
+            }
+        }
+
+        System.out.print(" = " + sumNumbers(numbersList));
+        System.out.println();
+    }
+
+    public static int minNumbers(List<Integer> numbersList) {
+        int min = 0;
+        for (Integer integer : numbersList) {
+            min = numbersList.get(0);
+            if (integer < min) {
+                min = integer;
+            }
+        }
+
+        return min;
+    }
+
+    public static void minNumbersView(List<Integer> numbersList) {
+        System.out.println("Najmniejsza liczba w liście to : " + minNumbers(numbersList));
+    }
+
+    public static int maxNumbers(List<Integer> numbersList) {
+        int max = 0;
+        for (Integer integer : numbersList) {
+            if (integer > max) {
+                max = integer;
+            }
+        }
+
+        return max;
+    }
+
+    public static void maxNumbersView(List<Integer> numbersList) {
+        System.out.println("Największa liczba w liście to : " + maxNumbers(numbersList));
+    }
+
+    public static boolean checkNumbersList(List<Integer> numbersList) {
+        if (numbersList.size() == 0) {
+            System.out.println("Lista jest pusta, brak wyników");
+            return false;
+        }
+        return true;
+    }
+}
